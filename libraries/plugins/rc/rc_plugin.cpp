995c53272388d8b0d9f1e9b2918425937052046a
theoreticalbts <theoreticalbts@users.noreply.github.com>
2018-06-29 11:09:30 -0400

    log exceptions

--- libraries/plugins/rc/rc_plugin.cpp
+++ libraries/plugins/rc/rc_plugin.cpp
@@ -44,6 +44,7 @@ class rc_plugin_impl
       void on_post_apply_transaction( const transaction_notification& note );
       void on_pre_apply_operation( const operation_notification& note );
       void on_post_apply_operation( const operation_notification& note );
+
       void on_first_block();
 
       database&                     _db;
@@ -574,6 +575,8 @@ struct post_apply_operation_visitor
    }
 };
 
+
+
 void rc_plugin_impl::on_pre_apply_operation( const operation_notification& note )
 {
    const dynamic_global_property_object& gpo = _db.get_dynamic_global_properties();
@@ -635,11 +638,16 @@ void rc_plugin::plugin_initialize( const boost::program_options::variables_map& 
 
       chain::database& db = appbase::app().get_plugin< steem::plugins::chain::chain_plugin >().db();
 
-      my->_post_apply_block_conn = db.add_post_apply_block_handler( [&]( const block_notification& note ){ my->on_post_apply_block( note ); }, *this, 0 );
-      //my->_pre_apply_transaction_conn = db.add_pre_apply_transaction_handler( [&]( const transaction_notification& note ){ my->on_pre_apply_transaction( note ); }, *this, 0 );
-      my->_post_apply_transaction_conn = db.add_post_apply_transaction_handler( [&]( const transaction_notification& note ){ my->on_post_apply_transaction( note ); }, *this, 0 );
-      my->_pre_apply_operation_conn = db.add_pre_apply_operation_handler( [&]( const operation_notification& note ){ my->on_pre_apply_operation( note ); }, *this, 0 );
-      my->_post_apply_operation_conn = db.add_post_apply_operation_handler( [&]( const operation_notification& note ){ my->on_post_apply_operation( note ); }, *this, 0 );
+      my->_post_apply_block_conn = db.add_post_apply_block_handler( [&]( const block_notification& note )
+         { try { my->on_post_apply_block( note ); } FC_LOG_AND_RETHROW() }, *this, 0 );
+      //my->_pre_apply_transaction_conn = db.add_pre_apply_transaction_handler( [&]( const transaction_notification& note )
+      //   { try { my->on_pre_apply_transaction( note ); } FC_LOG_AND_RETHROW() }, *this, 0 );
+      my->_post_apply_transaction_conn = db.add_post_apply_transaction_handler( [&]( const transaction_notification& note )
+         { try { my->on_post_apply_transaction( note ); } FC_LOG_AND_RETHROW() }, *this, 0 );
+      my->_pre_apply_operation_conn = db.add_pre_apply_operation_handler( [&]( const operation_notification& note )
+         { try { my->on_pre_apply_operation( note ); } FC_LOG_AND_RETHROW() }, *this, 0 );
+      my->_post_apply_operation_conn = db.add_post_apply_operation_handler( [&]( const operation_notification& note )
+         { try { my->on_post_apply_operation( note ); } FC_LOG_AND_RETHROW() }, *this, 0 );
 
       add_plugin_index< rc_resource_param_index >(db);
       add_plugin_index< rc_pool_index >(db);
